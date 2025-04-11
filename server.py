e5b535e53c2062c1c2d783beb037e76a0aa36f0d
n1ghtr33x <145277560+n1ghtr33x@users.noreply.github.com>
2025-04-11 02:34:55 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,61 @@
+import socket
+import threading
+
+HOST = '0.0.0.0'
+PORT = 8001
+
+# Класс игрока
+class Player:
+    def __init__(self, player_name):
+        self.player_name = player_name
+        self.id = None  # Уникальный идентификатор игрока
+
+    def __str__(self):
+        return f"Player(id={self.id}, name={self.player_name})"
+
+
+# Список для хранения игроков
+players = {}
+
+def handle_client(client_socket):
+    try:
+        # Принимаем запрос от клиента
+        message = client_socket.recv(1024).decode()
+        print(f"Received message: {message}")
+
+        if message.startswith("register|"):
+            player_name = message.split("|")[1]
+
+            player = Player(player_name)
+            player.id = len(players) + 1
+            players[player.id] = player
+            print(f"Player {player} created.")
+
+            # Отправляем подтверждение о регистрации
+            response = f"true"
+            client_socket.send(response.encode())
+        else:
+            # Для других сообщений
+            client_socket.send("Invalid request".encode())
+
+    except Exception as e:
+        print(f"Error handling client: {e}")
+    finally:
+        client_socket.close()
+
+
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(5)
+    print(f"Server started on {HOST}:{PORT}")
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f"Connection from {addr} has been established.")
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
+
+
+if __name__ == "__main__":
+    start_server()
